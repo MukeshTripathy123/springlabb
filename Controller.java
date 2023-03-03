89d6e1d43cb3005f39f748bc0d1242fd73fef47e
MukeshTripathy123 <117341431+MukeshTripathy123@users.noreply.github.com>
2023-03-03 13:56:00 +0530

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -17,28 +17,28 @@ import com.calculator.clac.Calculator;
 			return "<h1>The sum is "+ Calculator.addNo(num1, num2)+"</h1>";
 					  }
 		@GetMapping("/calculator/sub")
-		public String calculate1(@RequestParam(value = "n1",required = true,defaultValue = "1") int n1,
-				@RequestParam(value = "n2",required = true,defaultValue = "2" )int n2
+		public String calculate1(@RequestParam(value = "num1",required = true,defaultValue = "1") int num1,
+				@RequestParam(value = "num2",required = true,defaultValue = "2" )int num2
 				)
 		{
-		return "<h1>The substraction is "+ Calculator.subNo(n1, n2)+"</h1>";
+		return "<h1>The substraction is "+ Calculator.subNo(num1, nnum2)+"</h1>";
 		}
 		
 		@GetMapping("/calculator/multi")
-		public String calculate2(@RequestParam(value = "n1",required = true,defaultValue = "1") int n1,
-				@RequestParam(value = "n2",required = true,defaultValue = "2" )int n2
+		public String calculate2(@RequestParam(value = "num1",required = true,defaultValue = "1") int num1,
+				@RequestParam(value = "num2",required = true,defaultValue = "2" )int num2
 				)
 		{
-		return "<h1>The multiplication is "+ Calculator.multiNo(n1, n2)+"</h1>";
+		return "<h1>The multiplication is "+ Calculator.multiNo(num1, num2)+"</h1>";
 		
 		}
 		
 		@GetMapping("/calculator/div")
-		public String calculate3(@RequestParam(value = "n1",required = true,defaultValue = "1") int n1,
-				@RequestParam(value = "n2",required = true,defaultValue = "2" )int n2
+		public String calculate3(@RequestParam(value = "num1",required = true,defaultValue = "1") int num1,
+				@RequestParam(value = "num2",required = true,defaultValue = "2" )int num2
 				)
 		{
-		return "<h1>The division is "+ Calculator.divNo(n1, n2)+"</h1>";
+		return "<h1>The division is "+ Calculator.divNo(num1, num2)+"</h1>";
 		}
 	}
 		  
