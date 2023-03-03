f982c9e5a1950b9ef8b9ad8ade5cf6e0da19f1ae
MukeshTripathy123 <117341431+MukeshTripathy123@users.noreply.github.com>
2023-03-03 13:58:43 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -6,16 +6,16 @@ public static int addNo(int num1,int num2)
 return num1+num2;
 }
 
-public static int subNo(int x,int y)
+public static int subNo(int num1,int num2)
 {
-	return x-y;
+	return num1-num2;
 }
-public static double multiNo(double x,double y)
+public static double multiNo(double num1,double num2)
 {
-	return x*y;
+	return num1*num2;
 }
-public static double divNo(double x,double y)
+public static double divNo(double num1,double num2)
 {
 	
-	return x/y;    }
+	return num1/num2;    }
 }
